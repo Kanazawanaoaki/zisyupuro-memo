faf24bc8e0ad6d9d987e093223884d22558c99bb
Kanazawanaoaki <naoaki65k@gmail.com>
2019-12-19 16:39:59 +0900

    update arduino sketch .

--- arduino/2servo/2servo.ino
+++ arduino/2servo/2servo.ino
@@ -1,10 +1,11 @@
 #include <Servo.h>
 
 // サーボのピン番号
-const int SERVO_PIN = 8;
-
+const int SERVO_PIN1 = 8;
+const int SERVO_PIN2 = 9;
 // サーボのインスタンス
-Servo servo;
+Servo servo1;
+Servo servo2;
 
 // INITがtrueの時は、モーターの位置を0にするだけ
 const bool INIT = false;
@@ -14,27 +15,31 @@ const bool INIT = false;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);  
-  servo.attach(SERVO_PIN);
-  servo.write(0);
+  servo1.attach(SERVO_PIN1);
+  servo1.write(0);
+  servo2.attach(SERVO_PIN2);
+  servo2.write(0);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (INIT) return;
   Serial.print("start\n");
-  servo.write(0);
+  servo1.write(0);
   Serial.print("0\n");
   delay(2000);
-  servo.write(180);
+  servo1.write(180);
   Serial.print("180\n");
   delay(2000);
-  servo.write(90);
+  servo1.write(90);
   Serial.print("90\n");
   delay(2000);
-  servo.writeMicroseconds(1000);
+  servo1.writeMicroseconds(1000);
+  servo2.write(90);
   delay(2000);
-  servo.writeMicroseconds(2000);
+  servo1.writeMicroseconds(2000);
   delay(2000);
-  servo.writeMicroseconds(1500);
+  servo1.writeMicroseconds(1500);
+  servo2.write(180);
   delay(4000);
 }
